5a03b338b055e5d02f082e653b090ae682fe189a
Saks34 <119040050+Saks34@users.noreply.github.com>
2022-12-04 20:27:27 +0530

    Create sciprt.js

---
+++ sciprt.js
@@ -0,0 +1,7 @@
+let searchbar = document.querySelector('#search-bar')
+let searchbox = document.querySelector('#search-bar')
+
+
+searchbar.onclick = () = {
+  searchbox.classlist.toggle('active'
+}
